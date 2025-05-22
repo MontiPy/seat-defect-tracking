4ff97f513e7224b837cf9adb19de82bed37533e2
Michael Ontiveros <michaeltontiveros@gmail.com>
2025-05-22 13:51:51 -0500

    Added back button

--- frontend/src/pages/EntryDefectScreen.jsx
+++ frontend/src/pages/EntryDefectScreen.jsx
@@ -7,15 +7,17 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Button,
 } from "@mui/material";
 import api from "../services/api";
 import DefectMap from "../components/DefectMap";
 import DefectFormModal from "../components/DefectFormModal";
 import DefectList from "../components/DefectList";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function EntryDefectScreen() {
   const location = useLocation();
+  const navigate = useNavigate();
   const selectedProject = location.state?.project;
   const [images, setImages] = useState([]);
   const [selectedImage, setSelectedImage] = useState(null);
@@ -25,7 +27,7 @@ export default function EntryDefectScreen() {
   // load your 4 refs
   useEffect(() => {
     if (selectedProject) {
-      console.log('Selected Project:', selectedProject);
+      console.log("Selected Project:", selectedProject);
       api.get(`/images?project_id=${selectedProject}`).then((res) => {
         setImages(res.data);
         if (res.data.length) {
@@ -38,7 +40,14 @@ export default function EntryDefectScreen() {
   return (
     <Box sx={{ display: "flex", height: "100vh" }}>
       {/* LEFT 1/4: Image selector */}
-      <Box sx={{ width: "30%", bgcolor: "grey.100", p: 2, overflow: "auto" }}>
+      <Box sx={{ width: "30%", bgcolor: "grey.200", p: 2, overflow: "auto" }}>
+        <Button
+          variant="outlined"
+          sx={{ mb: 2, display: "block", textAlign: "center" }}
+          onClick={() => navigate("/")}
+        >
+          ← Back to Project Select
+        </Button>
         <Typography variant="h6">Image Selection</Typography>
         <Grid container spacing={2}>
           {images.slice(0, 4).map((img) => (
@@ -49,7 +58,7 @@ export default function EntryDefectScreen() {
                     component="img"
                     image={`${process.env.REACT_APP_API_URL}${img.url}`}
                     alt={img.filename}
-                    sx={{ height: 300, objectFit: "contain" }}
+                    sx={{ height: 200, objectFit: "contain" }}
                   />
                   <CardContent>
                     <Typography variant="body2" noWrap>
@@ -70,6 +79,8 @@ export default function EntryDefectScreen() {
           p: 2,
           overflow: "auto",
           borderRight: "1px solid black",
+          borderLeft: "1px solid black",
+          bgcolor: "grey.100",
         }}
       >
         <Typography variant="h6">Defect Map</Typography>
